154dc1d4932eb7db58b2c769fdcc5fe61907ccf2
domprechecopa <domprechecopa@gmail.com>
2019-03-02 17:36:58 -0300

    Todas as estrategias foram implementadas

--- helpers/strategy.py
+++ helpers/strategy.py
@@ -1,18 +1,18 @@
 import random
 class Strategy:
 
-    def __init__(self, select = 'best2bin', ppoints = None, cost = None):
+    def __init__(self, select = 'randbest1bin', ppoints = None, cost = None):
         strategy = {
             'rand1bin': self.rand1bin,
             'best1bin': self.best1bin,
             'best2bin': self.best2bin,
             'rand2bin': self.rand2bin,
-            # 'randbest1bin': self.randbest1bin,
-            # 'rand1exp' : self.rand1exp,
-            # 'best1exp' : self.best1exp,
-            # 'best2exp' : self.best2exp,
-            # 'rand2exp' : self.rand2exp,
-            # 'randbest1exp' : self.randbest2exp
+            'randbest1bin': self.randbest1bin,
+            'rand1exp' : self.rand1exp,
+            'best1exp' : self.best1exp,
+            'best2exp' : self.best2exp,
+            'rand2exp' : self.rand2exp,
+            'randbest1exp' : self.randbest2exp
         }
 
         self.strategy = strategy[select]
@@ -59,23 +59,51 @@ class Strategy:
                 
                 y.coords[iy] = x1[e].coords[iy] + F * (x[a].coords[iy] + x1[b].coords[iy] - x1[c].coords[iy] -  x1[d].coords[iy]  )
     
-    # def randbest1bin(self,x, y, x1 , CR,F):
-    #     [a,b,c,d,e] = ['a','b','c','d','e']
-    #     R = random.random() * x.dim
-    #     for iy in range(x.dim):
-    #         ri = random.random()
+    def randbest1bin(self,x, y, x1 , CR,F):
+        [a,b,c,d,e] = ['a','b','c','d','e']
+        R = random.random() * x.dim
+        for iy in range(x.dim):
+            ri = random.random()
 
-    #         if ri < CR or iy == R:
+            if ri < CR or iy == R:
                 
-    #             y.coords[iy] = y.coords[iy] + F * (x[a].coords[iy] + x1[b].coords[iy] - x1[c].coords[iy] -  x1[d].coords[iy]  )
-    
-    # def rand1exp(self,x, y, x1 , CR,F):
-    #     [a,b,c,d,e] = ['a','b','c','d','e']
-    #     R = random.random() * x.dim
-    #     for iy in range(x.dim):
-    #         ri = random.random()
+                y.coords[iy] = y.coords[iy] + F*(self.points[self.indexbest].coords[iy] - y.coords[iy]) + F * (x1[a].coords[iy] + x1[b].coords[iy])
 
-    #         if ri < CR or iy == R:
-                
-    #             y.coords[iy] = x1[e].coords[iy] + F * (x[a].coords[iy] + x1[b].coords[iy] - x1[c].coords[iy] -  x1[d].coords[iy]  )
+    def rand1exp(self,x, y, x1 , CR,F):
+        [a,b,c,d,e] = ['a','b','c','d','e']
+        R = random.random() * x.dim
+        for iy in range(x.dim):
+            ri = random.random()
+
+            if ri < CR or iy < x.dim:
+                y.coords[iy] = x1[c].coords[iy] + F * (x1[a].coords[iy] - x1[b].coords[iy])
+
+    def best1exp(self,x, y, x1, CR,F, ppoints = None):
+        [a,b,c,d,e] = ['a','b','c','d','e']
+        R = random.random() * x.dim
+        for iy in range(x.dim):
+            ri = random.random()
+
+            if ri < CR or iy < x.dim:
+                y.coords[iy] = self.points[self.indexbest].coords[iy] + F * (x1[a].coords[iy] - x1[b].coords[iy])
     
+
+    def best2exp(self,x, y, x1 , CR,F, ppoints = None):
+        [a,b,c,d,e] = ['a','b','c','d','e']
+        R = random.random() * x.dim
+        for iy in range(x.dim):
+            ri = random.random()
+
+            if ri < CR or iy < x.dim:
+                
+                y.coords[iy] = self.points[self.indexbest].coords[iy] + F * (x1[a].coords[iy] + x1[b].coords[iy] - x1[c].coords[iy] - x1[d].coords[iy])
+    
+    def randbest1exp(self,x, y, x1 , CR,F):
+        [a,b,c,d,e] = ['a','b','c','d','e']
+        R = random.random() * x.dim
+        for iy in range(x.dim):
+            ri = random.random()
+
+            if ri < CR or iy < x.dim:
+                
+                y.coords[iy] = y.coords[iy] + F*(self.points[self.indexbest].coords[iy] - y.coords[iy]) + F * (x1[a].coords[iy] + x1[b].coords[iy])
